759738ef3c12083a3600419d3a68fd3192f53ffe
pmilowski <pmilowski16@gmail.com>
2023-03-29 17:44:03 +0200

    Updated CreateCommand

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,8 +40,19 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
+            BaseGradeBook gradeBook;
+            if (parts[2] == "standard")
+            {
+                gradeBook = new StandardGradeBook(name);
+            } else if (parts[2] == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            } else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", parts[2]);
+                return;
+            }
+            // Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
