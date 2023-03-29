f9386a86056bec78775bcd1ee29e1dae3b27833f
pmilowski <pmilowski16@gmail.com>
2023-03-29 17:33:18 +0200

    GetLetterGrade + statistics
    CalculateStatistics
    CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,38 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5) { throw new InvalidOperationException(); }
+            int avgGradeCount = 0;
+            int students20 = (int)(Students.Count * 0.2f);
+
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    avgGradeCount++;
+                }
+            }
+
+            if (avgGradeCount < students20) return 'A';
+            else if (avgGradeCount < 2 * students20) return 'B';
+            else if (avgGradeCount < 3 * students20) return 'C';
+            else if (avgGradeCount < 4 * students20) return 'D';
+            else return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) { Console.WriteLine("Ranked grading requires at least 5 students."); }
+            else base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) { Console.WriteLine("Ranked grading requires at least 5 students."); }
+            else base.CalculateStudentStatistics(name);
+        }
+
     }
 }
